82869583f1a2426b5003682cae30f4d6a701150d
Carlo Contardi <c.contardi@spacecoding.it>
2024-11-29 18:04:35 +0100

    add a Layout

--- app/src/main/java/it/spacecoding/magicball8/MainActivity.java
+++ app/src/main/java/it/spacecoding/magicball8/MainActivity.java
@@ -2,23 +2,36 @@ package it.spacecoding.magicball8;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageView ballDisplay = findViewById(R.id.image_eightBall);
+        int[] ballArray = {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        Button myButton = findViewById(R.id.askButton);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        myButton.setOnClickListener(v -> {
+            Random randomNumberGenerator = new Random();
+            int number = randomNumberGenerator.nextInt(4);
+            ballDisplay.setImageResource(ballArray[number]);
         });
     }
 }
